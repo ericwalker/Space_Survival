5d68b4ac625dbe6af14a839669fdee638986fcc0
ericwalker <ericdavid78929@gmail.com>
2018-01-03 20:51:41 +0800

    Add scripts
    DamageManager.cs
    PlayerMovement.cs

---
+++ Space Survival Project/Assets/Scripts/DamageManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageManager : MonoBehaviour {
+
+	public int health = 1;
+
+	float invulnTimer = 0; // use to countdown for the invulnerable time
+	int correctLayer;
+
+	void Start(){
+		correctLayer = gameObject.layer;
+	}
+
+	void OnTriggerEnter2D(){
+		Debug.Log ("Trigger");
+
+		health--;
+		invulnTimer = 2f;
+
+		gameObject.layer = 10;
+	}
+
+	void Update(){
+
+		invulnTimer -= Time.deltaTime;
+
+		if (invulnTimer <= 0) {
+			gameObject.layer = correctLayer;
+		}
+
+		if (health <= 0) {
+			Die ();
+		}
+	}
+
+	void Die(){
+	
+		Destroy (gameObject);
+	}
+}

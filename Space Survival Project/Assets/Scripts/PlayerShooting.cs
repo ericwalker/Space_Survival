5bd165e68afa118e1cdac375ea75615257db2a1b
ericwalker <ericdavid78929@gmail.com>
2018-01-03 21:02:18 +0800

    Add playerShooting.cs

---
+++ Space Survival Project/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+
+	public float fireDelay = 0.25f;
+	float coolDownTimer = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// when the space is pressed down
+		if (Input.GetButton ("Fire1") && coolDownTimer <= 0) {
+			// SHOOT the bullets
+			coolDownTimer = fireDelay;
+		}
+
+	}
+}

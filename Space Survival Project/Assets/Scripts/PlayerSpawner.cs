515e69e1414f71be8b16170b644a869069c3368f
ericwalker <ericdavid78929@gmail.com>
2018-02-02 17:40:47 +0800

    add OnGUI() in  PlayerSpawner.cs

--- Space Survival Project/Assets/Scripts/PlayerSpawner.cs
+++ Space Survival Project/Assets/Scripts/PlayerSpawner.cs
@@ -24,7 +24,7 @@ public class PlayerSpawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (playerInstance == null) {
+		if (playerInstance == null && numLives > 0) {
 			respawnTimer -= Time.deltaTime;
 
 			if (respawnTimer <= 0) {
@@ -34,6 +34,10 @@ public class PlayerSpawner : MonoBehaviour {
 	}
 
 	void OnGUI(){
-		
+		if (numLives > 0 || playerInstance != null) {
+			GUI.Label (new Rect (0, 0, 100, 50), "Lives Left: " + numLives);
+		} else {
+			GUI.Label (new Rect (Screen.width/2 - 50, Screen.height/2 - 25, 100, 50), "Game Over");
+		}
 	}
 }
